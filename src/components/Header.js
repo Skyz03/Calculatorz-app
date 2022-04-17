5936d15ffa41fe5d69658ff4772df6f9eba95729
Skyz03 <aakibdiscoo@gmail.com>
2022-04-17 12:38:25 +0545

    Updates in the header sass

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,56 @@ import React from "react";
 const Header = () => {
   return (
     <header className="calc__header">
-      <h1 className="calc__title">Calculator</h1>
+      <h1 className="calc__title">Calc</h1>
+      <div className="calc__switch-wrapper">
+        <span className="calc__text">Theme</span>
+        <div className="calc__switch">
+          {/* Input 01 */}
+          <label className="calc__label" for="dark-theme">
+            1
+          </label>
+          <input
+            className="calc__input"
+            type="radio"
+            id="dark-theme"
+            name="theme switch"
+            data-theme-input="dark"
+          />
+          {/* Input 02 */}
+          <label className="calc__label" for="light-theme">
+            2
+          </label>
+          <input
+            className="calc__input"
+            type="radio"
+            id="light-theme"
+            name="theme switch"
+            data-theme-input="light"
+          />
+          {/* Input 03 */}
+          <label className="calc__label" for="neon-theme">
+            3
+          </label>
+          <input
+            className="calc__input"
+            type="radio"
+            id="neon-theme"
+            name="theme switch"
+            data-theme-input="neon"
+          />
+          {/* Input 03 */}
+          <label className="calc__label" for="custom-theme">
+            4
+          </label>
+          <input
+            className="calc__input"
+            type="radio"
+            id="custom-theme"
+            name="theme switch"
+            data-theme-input="custom"
+          />
+        </div>
+      </div>
     </header>
   );
 };
